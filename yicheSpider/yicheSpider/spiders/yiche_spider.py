8c21add498259d260997856f2a18efbbc303cc8d
123276890 <123276890@qq.com>
2018-11-12 18:39:44 +0800

    易车网

--- yicheSpider/yicheSpider/spiders/yiche_spider.py
+++ yicheSpider/yicheSpider/spiders/yiche_spider.py
@@ -3,3 +3,17 @@
 import scrapy
 import re
 from ..items import YichespiderItem
+from scrapy_splash import SplashRequest
+
+
+class yicheSpider(scrapy.Spider):
+    name = "yiche"
+    allowed_domains = ["www.bitauto.com"]
+    url = 'http://car.bitauto.com/tree_chexing'
+
+    def start_requests(self):
+        yield scrapy.Request(self.url, callback=self.parse, meta={'splash': {'args':{'endpoint':'render.html'}}})
+
+    def parse(self, response):
+        for sel in response.xpath('body'):
+            item = YichespiderItem()
