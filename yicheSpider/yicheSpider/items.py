abd044104584007e3477f7791bdb23566142b0d4
123276890 <123276890@qq.com>
2018-11-13 10:53:37 +0800

    易车网

--- yicheSpider/yicheSpider/items.py
+++ yicheSpider/yicheSpider/items.py
@@ -46,6 +46,110 @@ class YichespiderItem(scrapy.Item):
     gears_type = scrapy.Field()                    # 变速箱类型
     gears_num = scrapy.Field()                     # 挡位个数
     drive_method = scrapy.Field()                  # 驱动方式
+    susp_front_type = scrapy.Field()               # 前悬架类型
+    susp_back_type = scrapy.Field()                # 后悬架类型
+    variable_susp = scrapy.Field()                 # 可调悬架
+    front_brake = scrapy.Field()                   # 前轮制动器类型
+    back_brake = scrapy.Field()                    # 后轮制动器类型
+    park_brake = scrapy.Field()                    # 驻车制动类型
+    structure = scrapy.Field()                     # 车体结构
+    front_diff_lock = scrapy.Field()               # 限滑差速器/差速锁
+    anti_lock = scrapy.Field()                     # ABS防抱死
+    bfd = scrapy.Field()                           # 制动力分配(EBD/CBC等)
+    bas = scrapy.Field()                           # 制动辅助(EBA/BAS/BA等)
+    tcs = scrapy.Field()                           # 牵引力控制(ASR/TCS/TRC等)
+    stable_control = scrapy.Field()                # 车身稳定控制(ESC/ESP/DSC等)
+    seat_srs = scrapy.Field()                      # 主/副驾驶座安全气囊
+    side_airbag = scrapy.Field()                   # 前/后排侧气囊
+    head_srs = scrapy.Field()                      # 侧安全气帘
+    knee_srs = scrapy.Field()                      # 膝部气囊
+    seat_air_bag = scrapy.Field()                  # 安全带气囊
+    central_air_bag = scrapy.Field()               # 后排中央气囊
+    tire_pres_monitor = scrapy.Field()             # 胎压监测
+    zero_tire_pres = scrapy.Field()                # 零胎压继续行驶
+    isofix = scrapy.Field()                        # 后排儿童座椅接口
+    cruise_ctrl = scrapy.Field()                   # 定速巡航
+    ldw = scrapy.Field()                           # 车道保持
+    bsa = scrapy.Field()                           # 并线辅助
+    abs = scrapy.Field()                           # 主动刹车/碰撞报警
+    tired_drive = scrapy.Field()                   # 疲劳提醒
+    auto_park_in = scrapy.Field()                  # 自动泊车
+    control_park_in = scrapy.Field()               # 遥控泊车
+    auto_drive = scrapy.Field()                    # 自动驾驶辅助
+    auto_park = scrapy.Field()                     # 自动驻车
+    hac = scrapy.Field()                           # 上坡辅助
+    hdc = scrapy.Field()                           # 陡坡缓降
+    nvs = scrapy.Field()                           # 夜视系统
+    vgrs = scrapy.Field()                          # 可变齿比转向
+    radar = scrapy.Field()                         # 前/后驻车雷达
+    reverse_video = scrapy.Field()                 # 倒车影像
+    drive_model = scrapy.Field()                   # 驾驶模式选择
+    headlamps = scrapy.Field()                     # 前大灯
+    led_beam = scrapy.Field()                      # LED日间行车灯
+    automatic_headlight = scrapy.Field()           # 自动大灯
+    front_fog_lamp = scrapy.Field()                # 前雾灯
+    light_height_adjt = scrapy.Field()             # 大灯高度可调
+    light_clean_dev = scrapy.Field()               # 大灯清洗装置
+    skylight_type = scrapy.Field()                 # 天窗类型
+    power_window = scrapy.Field()                  # 前/后电动车窗
+    e_adjt_rearview = scrapy.Field()               # 后视镜电动调节
+    heat_rearview = scrapy.Field()                 # 后视镜加热
+    mirror_mem = scrapy.Field()                    # 后视镜记忆
+    power_mirror = scrapy.Field()                  # 电动折叠
+    dimming_mirror = scrapy.Field()                # 内/外后视镜自动防眩目
+    stream_media_rearview = scrapy.Field()         # 流媒体后视镜
+    side_priv_glass = scrapy.Field()               # 隐私玻璃
+    side_abat_vent = scrapy.Field()                # 后排侧遮阳帘
+    abat_vent = scrapy.Field()                     # 后遮阳帘
+    before_the_wiper = scrapy.Field()              # 前雨刷器
+    after_the_wiper = scrapy.Field()               # 后雨刷器
+    electric_door_absorption = scrapy.Field()      # 电吸门
+    slide_door = scrapy.Field()                    # 电动侧滑门
+    electric_suitcase = scrapy.Field()             # 电动行李箱
+    roof_rack = scrapy.Field()                     # 车顶行李架
+    e_ctrl_lock = scrapy.Field()                   # 中控锁
+    intelligent_key = scrapy.Field()               # 智能钥匙
+    remote_control = scrapy.Field()                # 远程遥控功能
+    tail = scrapy.Field()                          # 尾翼
+    sport_package = scrapy.Field()                 # 运动外观套件
+    interior_materials = scrapy.Field()            # 内饰材质
+    interior_atmosphere_light = scrapy.Field()     # 车内氛围灯
+    sun_shield = scrapy.Field()                    # 遮阳板化妆镜
+    steering_material = scrapy.Field()             # 方向盘材质
+    functional_steer = scrapy.Field()              # 多功能方向盘
+    steer_adjt = scrapy.Field()                    # 方向盘调节
+    steer_heat = scrapy.Field()                    # 方向盘加热
+    steer_shift = scrapy.Field()                   # 方向盘换挡
+    before_air = scrapy.Field()                    # 前排空调
+    back_air = scrapy.Field()                      # 后排空调
+    sweet_system = scrapy.Field()                  # 香氛系统
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
